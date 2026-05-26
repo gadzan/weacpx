3d7343bb6a41bac0c31391cc384864e34f362f79
Gadzan Mak <gadzan@qq.com>
2026-05-26 21:32:27 +0800

    chore: bump core version to 0.6.0

--- tests/unit/packages/package-metadata.test.ts
+++ tests/unit/packages/package-metadata.test.ts
@@ -16,10 +16,10 @@ test("root package publishes as weacpx and exposes plugin-api", () => {
   });
 });
 
-test("root package version is 0.5.2", () => {
+test("root package version is 0.6.0", () => {
   const pkg = readJson("package.json");
 
-  expect(pkg.version).toBe("0.5.2");
+  expect(pkg.version).toBe("0.6.0");
 });
 
 test("first-party channel plugins peer depend on weacpx", () => {
